d177cba1c36a40ad7c1d6e105427c41f8399cefa
yk3372 <yk3372@gmail.com>
2020-02-23 16:31:00 +0800

    update code
    Signed-off-by: yk3372 <yk3372@gmail.com>

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import media_saver
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  MediaSaverPlugin.register(with: registry.registrar(forPlugin: "MediaSaverPlugin"))
 }
